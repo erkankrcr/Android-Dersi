da9a40069b127277887520d4a3d5d16dd1ae9e77
erkan <erkan@ThinkPad-X240>
2019-03-19 11:42:27 +0300

    Hafta Dört Gördüklerim

---
+++ https:/github.com/erkankrcr/Android-Dersi.git/HaftaDort/src/Circle.java
@@ -0,0 +1,19 @@
+
+public class Circle {
+	public double x;
+	public double y;
+	public double r;
+	
+	public Circle() {
+		this(1,1,1);
+	}
+	public Circle(double x , double y , double r) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	public double Cevre() {
+		return 2*3*r;
+	}
+
+}
---
+++ https:/github.com/erkankrcr/Android-Dersi.git/HaftaDort/src/Start.java
@@ -0,0 +1,14 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Circle c = new Circle();
+		
+		System.out.println(c.x);
+		System.out.println(c.Cevre());
+
+	}
+
+}
